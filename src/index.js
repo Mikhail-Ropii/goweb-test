7c563520e7d4b60db0362300d30817f55f6c8a1f
Mikhail-Ropii <mr.ropiy@gmail.com>
2023-01-30 09:11:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -34,7 +34,18 @@ contactForm.addEventListener('submit', function (e) {
 
   if (!email.value) {
     errorMessage.classList.add('visible');
+    return;
   }
+  const myForm = e.target;
+  const formData = new FormData(myForm);
+
+  fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log('Form successfully submitted'))
+    .catch(error => alert(error));
 });
 
 email.addEventListener('input', function (e) {
